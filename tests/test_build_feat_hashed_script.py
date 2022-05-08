db8d0f073117fd3dd91a2aff95082476271a1db7
Timothy Moore <mtimothy984@gmail.com>
2022-05-08 09:45:13 -0700

    Use universal comment style for sample js file

--- tests/test_build_feat_hashed_script.py
+++ tests/test_build_feat_hashed_script.py
@@ -12,7 +12,7 @@ from vanillaplusjs.constants import PROCESSOR_VERSION
 # using the """ syntax.
 BASIC = {
     "orig": {
-        "src/public/js/index.js": "// we don't need a real script here for this test",
+        "src/public/js/index.js": "/* we don't need a real script here for this test */",
         "src/public/index.html": """
 <!DOCTYPE html>
 <html>
@@ -27,13 +27,13 @@ BASIC = {
 """,
     },
     "conv": {
-        "out/www/js/index.js": "// we don't need a real script here for this test",
+        "out/www/js/index.js": "/* we don't need a real script here for this test */",
         "out/www/index.html": """<!DOCTYPE html><html><head><meta charset="utf-8">
     <title>Test</title>
 </head>
 <body>
     <div class="bg-white">White background</div>
-    <script src="/js/index.js?v=M-8fgtHgydg9CPdmOzAvjGPnKJ8bOWmOpWU7qP9AcDw=&amp;pv={PROCESSOR_VERSION}" type="module"></script>
+    <script src="/js/index.js?v=miPr_dZeaCM4PeNb2iCfcKwSMOI5twWfEsyxMep1Pqc=&amp;pv={PROCESSOR_VERSION}" type="module"></script>
 
 
 </body></html>
