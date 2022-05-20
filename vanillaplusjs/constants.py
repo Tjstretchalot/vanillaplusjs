f7ff31e493a12d722d176bfba3c5e29cc917d417
Timothy Moore <mtimothy984@gmail.com>
2022-05-20 07:16:16 -0700

    Version 0.1.5 - version via attr

--- vanillaplusjs/constants.py
+++ vanillaplusjs/constants.py
@@ -5,12 +5,5 @@ CONFIGURATION_VERSION = "1.0.0"
 automatically migrate old configuration files.
 """
 
-PROCESSOR_VERSION = "1"
+PROCESSOR_VERSION = "0.1.15"
 """The version of vanillaplusjs we're using"""
-
-
-with open(os.path.abspath(os.path.join(__file__, "../../setup.cfg")), "r") as f:
-    for line in f:
-        line: str
-        if line.startswith("version = "):
-            PROCESSOR_VERSION = line.split("=")[1].strip()
