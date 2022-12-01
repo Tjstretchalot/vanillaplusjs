a57f47c3abc311a370e206d2b4a20d17759b604c
Timothy Moore <mtimothy984@gmail.com>
2022-12-01 11:11:45 -0800

    Version 1.2.2

--- vanillaplusjs/constants.py
+++ vanillaplusjs/constants.py
@@ -3,5 +3,5 @@ CONFIGURATION_VERSION = "1.0.0"
 automatically migrate old configuration files.
 """
 
-PROCESSOR_VERSION = "1.2.1"
+PROCESSOR_VERSION = "1.2.2"
 """The version of vanillaplusjs we're using"""
