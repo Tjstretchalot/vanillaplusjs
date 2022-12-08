41469bc31d655b890d31f7f028c7627456c37c95
Timothy Moore <mtimothy984@gmail.com>
2022-12-08 15:27:00 -0800

    Prompt to clean placeholders

--- tests/test_clean.py
+++ tests/test_clean.py
@@ -23,6 +23,36 @@ class Test(unittest.TestCase):
         finally:
             shutil.rmtree("tmp")
 
+    def test_removes_placeholder(self):
+        os.makedirs(os.path.join("tmp"), exist_ok=True)
+        try:
+            vanillaplusjs.runners.init.main(["--folder", "tmp"])
+            os.makedirs(os.path.join("tmp", "src", "public", "js"), exist_ok=True)
+            with open(
+                os.path.join("tmp", "src", "public", "js", "foo.images.json"), "w"
+            ) as f:
+                f.write("{}")
+            vanillaplusjs.runners.build.main(["--folder", "tmp"])
+            self.assertTrue(
+                os.path.exists(os.path.join("tmp", "out", "www", "index.html"))
+            )
+            self.assertTrue(
+                os.path.exists(
+                    os.path.join("tmp", "src", "public", "js", "foo.images.js")
+                )
+            )
+            vanillaplusjs.runners.clean.main(["--folder", "tmp", "--placeholders"])
+            self.assertFalse(
+                os.path.exists(os.path.join("tmp", "out", "www", "index.html"))
+            )
+            self.assertFalse(
+                os.path.exists(
+                    os.path.join("tmp", "src", "public", "js", "foo.images.js")
+                )
+            )
+        finally:
+            shutil.rmtree("tmp")
+
 
 if __name__ == "__main__":
     unittest.main()
